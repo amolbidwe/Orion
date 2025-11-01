77c4627dcf9b158d191d9f01de106d84a2188709
amolbidwe <amolrbidwe@gmail.com>
2025-11-02 02:54:48 +0530

    Update nested.py

--- 1_python_basics/2. Conditional statements/nested.py
+++ 1_python_basics/2. Conditional statements/nested.py
@@ -1,1 +1,14 @@
+num = 45
+if (num < 0):
+    print("Number is negative.")
 
+elif (num > 0):
+    if(num <= 30):
+        print("Number is less than 30")
+    elif (num > 30 and num <= 50):
+        print("Number is between 30-50")
+    else:
+        print("Number is greater than 50")
+        
+else:
+    print("Number is zero")
