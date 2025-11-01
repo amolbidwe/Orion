5c054e539d1276a0ec7433ca2ff924b6c95c33e9
amolbidwe <amolrbidwe@gmail.com>
2025-11-02 02:29:03 +0530

    Update Data_types.py

--- 1_python_basics/1. Print, Variables and Data types/Data_types.py
+++ 1_python_basics/1. Print, Variables and Data types/Data_types.py
@@ -1,1 +1,15 @@
-
+a=1
+a1=9.1
+b="Amol"
+c=True
+d=None
+e= complex(18,45)
+print(a)
+print(b)
+print(a+a1)
+print("The type of a is", type(a))
+print("The type of b is", type(b))
+print("The type of c is", type(c))
+print("The type of d is", type(d))
+print("The type of a1 is",type(a1))
+print("The type of e is", type(e))
