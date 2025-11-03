f92c326f581796664013b3cf190975c304d55192
amolbidwe <amolrbidwe@gmail.com>
2025-11-04 00:38:13 +0530

    Update sets.py

--- 3_data_structures/Sets/sets.py
+++ 3_data_structures/Sets/sets.py
@@ -1,1 +1,17 @@
+# sets
+# Sets are unorderd collection of data items. They store multiple items in a single variable>
+# Set items are seprated by commas and enclosed within curly brackets{}. Sets are unchangable.
 
+s = {2, 4, 2, 6}
+print(s) 
+
+info = {"Vishwa", 19, False, 5.9, 45}
+print(info)
+# Items of the set occur in random order and hence they cannot be accessed using index number
+
+# empty set
+random = set()
+print(type(random))
+
+for value in info:
+    print(value)
