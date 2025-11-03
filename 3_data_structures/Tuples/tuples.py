f45c7a6ec8881ae16618cd4cb37c6a911dc234d1
amolbidwe <amolrbidwe@gmail.com>
2025-11-04 00:48:51 +0530

    Update tuples.py

--- 3_data_structures/Tuples/tuples.py
+++ 3_data_structures/Tuples/tuples.py
@@ -1,1 +1,20 @@
+#tuple 
+# Tuples are unchangeable
+# It is the ordered collection of data items. They store multiple items in a single variable. Tuples are separated by comma and enclosed within round bracket. 
+tup = (1, 5, 6, "Violet", 741)
+print(type(tup), tup)
+print(tup[0])
+print(tup[1])
+print(tup[2])
+print(tup[3])
 
+if 741 in tup:
+    print("Yes 741 is present in this tuple")
+else:
+    print("342 is not present in tuple")
+# Here we can do slicing and after slicing new tuple is formed as tuple is unchangeable.
+
+tup2 = tup[1 : 4]
+print(tup2)
+tup3 = tup[1 : 5 : 2]
+print(tup3)
