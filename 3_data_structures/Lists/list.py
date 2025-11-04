a364645d243fb54f5f2a7fc06e91b7462bbe77f2
Sahil Sawant <sahil.asawant30@gmail.com>
2025-11-04 12:34:18 +0530

    Refactor list.py with pulsar examples and updates
    Updated pulsar list operations and added examples for list comprehension.

--- 3_data_structures/Lists/list.py
+++ 3_data_structures/Lists/list.py
@@ -1,37 +1,21 @@
-# python list
-# It is ordered collection of data items
-# they store multiple itens in single variable
-# list items are changable meanings we can alter them after creation
-l = [ 3, 5 , 6 ,"Vishwa",True] # list may be consist of multiple data types
-#    [0] [1] [2]  [3]    [4]    index  
-print(l)
-print(l[:])
-print(l[1:4])
-print(l[1:4:2])  # here 2 is jump index 
-print(type(l))
-print(l[0])
-print(l[1])
-print(l[2])
+# list of pulsars observed
+pulsars = ["Crab", "Vela", "Geminga"]
 
-print(l[-3])
-print(l[len(l)-3])
-print(l[5-3])
-print(l[2])
+print("First pulsar:", pulsars[0])
 
+# add new detected pulsar
+pulsars.append("PSR B1937+21")
 
-if "Vishwa" in l:
-    print("Yes")
-else:
-    print("No")
+# remove a pulsar
+pulsars.remove("Vela")
 
-if "wa" in "Vishwa":
-    print("Yes")
-else:
-    print("No")
-    
-lst = [i*i for i in range(10)]
-print(lst)
-lst = [i*i for i in range(10) if i%2]
-print(lst)
-lst = [i*i for i in range(4)]
-print(lst)
+# update a name (example correction)
+pulsars[1] = "Geminga Pulsar"
+
+# list comprehension: convert names to uppercase
+upper_pulsars = [p.upper() for p in pulsars]
+print("Uppercase:", upper_pulsars)
+
+# iterate
+for p in pulsars:
+    print("Observed:", p)
